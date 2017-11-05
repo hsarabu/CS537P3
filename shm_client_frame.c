f3314762ec351752e2f41d064839eaaf55f15182
Hitesh <hsarabu@wisc.edu>
2017-11-04 20:17:05 -0500

    Starting to debug

--- shm_client_frame.c
+++ shm_client_frame.c
@@ -4,18 +4,39 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/mman.h>
+#include <signal.h>
+#include <memory.h>
 // ADD NECESSARY HEADERS
 
+#define SHM_NAME "hitesh_bolka"
+#define NULL 0
 // Mutex variables
 pthread_mutex_t* mutex;
+void* address;
+typedef struct {
+    int pid;
+    char birth[25];
+    char clientString[10];
+    int elapsed_sec;
+    double elapsed_msec;
+    time_t start_time;
+} stats_t;
+
+int i = 0;
 
 void exit_handler(int sig) {
-    // ADD
-
     // critical section begins
 	pthread_mutex_lock(mutex);
-
-    // Client leaving; needs to reset its segment   
+    //load pid
+    ((stats_t*)(address + i * sizeof(stats_t)))->pid = NULL;
+    //load string
+    strcpy(((stats_t*)(address + i * sizeof(stats_t)))->clientString, NULL);
+    //load start UNIX stamp
+    ((stats_t*)(address + i * sizeof(stats_t)))->start_time = NULL ;
+    //prime sec and msec
+    ((stats_t*)(address + i * sizeof(stats_t)))->elapsed_sec = NULL;
+    ((stats_t*)(address + i * sizeof(stats_t)))->elapsed_msec = NULL;
+    strcpy(((stats_t*)(address + i * sizeof(stats_t)))->birth, NULL);
 
 	pthread_mutex_unlock(mutex);
 	// critical section ends
@@ -25,22 +46,63 @@ void exit_handler(int sig) {
 
 int main(int argc, char *argv[]) {
 	// ADD    
-	
+    int fd_shm = shm_open(SHM_NAME, O_RDWR, 0660);
+    address = mmap(NULL, getpagesize(), PROT_READ|PROT_WRITE,MAP_SHARED, fd_shm, 0);
+    struct sigaction act;
+    act.sa_sigaction = &exit_handler;
+    act.sa_flags = SA_SIGINFO;
+    if(sigaction(SIGTERM|SIGINT, &act, NULL) < 0){
+        perror("sigaction error");
+        return 1;
+    }
+    int curr_pid = getpid();
+    int max_clients = getpagesize()/ sizeof(stats_t) - 1;
     // critical section begins
     pthread_mutex_lock(mutex);
-        
 	// client updates available segment
-		
+    time_t curr_time = time(NULL);
+    int index = 0;
+    for(int i = 0; i < max_clients; i++){
+        if ((address + i* sizeof(stats_t)) == NULL){
+            //load pid
+            ((stats_t*)(address + i * sizeof(stats_t)))->pid = curr_pid;
+            //load string
+            strcpy(((stats_t*)(address + i * sizeof(stats_t)))->clientString, argv[1]);
+            //load start UNIX stamp
+            ((stats_t*)(address + i * sizeof(stats_t)))->start_time = curr_time ;
+            //prime sec and msec
+            ((stats_t*)(address + i * sizeof(stats_t)))->elapsed_sec = 0;
+            ((stats_t*)(address + i * sizeof(stats_t)))->elapsed_msec = 0;
+            strcpy(((stats_t*)(address + i * sizeof(stats_t)))->birth, ctime(&curr_time));
+            index = i;
+        }
+    }
+
 	pthread_mutex_unlock(mutex);
     // critical section ends
-        
+    i = index; //useful for when we exit, i know, bad use of i var
 	while (1) {
-        
-		// ADD
-        
-        sleep(1);
 
+        double elapsed_time = difftime(time(NULL), curr_time);
+        //truncating to get s
+        int elapsed_seconds = (int)elapsed_time;
+        ((stats_t*)(address + index * sizeof(stats_t)))->elapsed_sec = elapsed_seconds;
+        ((stats_t*)(address + index * sizeof(stats_t)))->elapsed_msec = (elapsed_time - elapsed_seconds)/1000;
+        sleep(1);
 		// Print active clients
+        int active_clients[max_clients];
+        int active_clients_counter = 0;
+        for(int i = 0; i < max_clients; i++){
+            if ((address + i* sizeof(stats_t)) != NULL){
+                active_clients[active_clients_counter] = ((stats_t*)(address + i * sizeof(stats_t)))->pid;
+                active_clients_counter++;
+            }
+        }
+        printf("Active Clients :");
+        for(int i = 0; i < active_clients_counter; i++){
+            printf(" %i", active_clients[i]);
+        }
+        printf("\n");
     }
 
     return 0;
--- shm_server_frame.c
+++ shm_server_frame.c
@@ -4,29 +4,34 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/mman.h>
+#include <time.h>
+#include <memory.h>
+#include <signal.h>
 // ADD NECESSARY HEADERS
 
 
 // Shared memory name: hitesh_bolkai
 #define SHM_NAME "hitesh_bolka"
-
+#define NULL 0
 // Mutex variables
 pthread_mutex_t* mutex;
 pthread_mutexattr_t mutexAttribute;
 void* address;
-
-typedef stack_t struct {
+typedef struct {
 	int pid;
 	char birth[25];
 	char clientString[10];
 	int elapsed_sec;
 	double elapsed_msec;
+	long unix_start_stamp;
 } stats_t;
+void print_line(stats_t *client, int iteration);
 
-void exit_handler(int sig) 
+void exit_handler(int sig)
 {
     // ADD
-    
+    munmap(address, getpagesize()); //TODO: Check if this is right
+	shm_unlink(SHM_NAME);
 	exit(0);
 }
 
@@ -35,23 +40,40 @@ int main(int argc, char *argv[])
     // ADD
 	//TODO: Error Handling
 	// Creating a new shared memory segment
-	int fd_shm = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0660);	
+	int fd_shm = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0660);
+	if(fd_shm == NULL) return 1;
 	ftruncate(fd_shm, getpagesize());
-	address = mmap(NULL, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd_shm, 0);        
-
-	int max_clients = getpagesize()/64 - 1;
-			
+	address = mmap(NULL, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd_shm, 0);
+	int max_clients = getpagesize()/sizeof(stats_t) - 1;
+	struct sigaction act;
+	act.sa_sigaction = &exit_handler;
+	act.sa_flags = SA_SIGINFO;
+	if(sigaction(SIGINT|SIGTERM, &act, NULL) < 0) {
+		perror("sigaction error");
+		return 1;
+	}
+    //stats_t *client_stats = malloc(sizeof(stats_t)*max_clients);
+	stats_t client_stats[max_clients];
+	memcpy(fd_shm, &client_stats);
     // Initializing mutex
 	pthread_mutexattr_init(&mutexAttribute);
 	pthread_mutexattr_setpshared(&mutexAttribute, PTHREAD_PROCESS_SHARED);
 	pthread_mutex_init(mutex, &mutexAttribute);
-	
+	int curr_iteration = 0;
     while (1) 
 	{
-		// ADD
-        break;
+		for(int i = 0; i < max_clients; i++){
+			if((address + i* sizeof(stats_t)) != NULL){
+				print_line(((stats_t*)(address + i * sizeof(stats_t))), curr_iteration);
+			}
+		}
         sleep(1);
+        curr_iteration++;
     }
 
     return 0;
+}
+
+void print_line(stats_t *client, int iteration){
+	printf("%i, pid : %i , birth : %s, elapsed : %i s %d0 ms, %s",iteration, client->pid, client->birth, client->elapsed_sec, client->elapsed_msec, client->clientString);
 }
